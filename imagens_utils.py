9579cc44f378a0b746b5e14552ec74d19fd53eec
Christhian Souza <christhian.souza@veolia.com>
2022-04-19 15:13:26 -0300

    STUUUF

--- imagens_utils.py
+++ imagens_utils.py
@@ -33,24 +33,11 @@ def tratarImagensAdaptativo(origem = None,destino =None):
     for arquivo in arquivos:
         imagem = cv2.imread(arquivo,0)
         imagem = cv2.GaussianBlur(imagem,(5,7),5)
-        #transformar em escala de cinza
-        # imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_RGB2GRAY)
-        # _, imagem_tratada = cv2.threshold(imagem_cinza,99,255, cv2.THRESH_TRUNC or cv2.THRESH_OTSU)
+        
         imagem_tratada = cv2.adaptiveThreshold(imagem,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,661,138)
         nome_arquivo = os.path.basename(arquivo)
         cv2.imwrite(f'{destino}/{nome_arquivo}',imagem_tratada)
-    # arquivos = glob.glob(f"{destino}/*")
-    # for arquivo in arquivos:
-    #     imagem = Image.open(arquivo)
-    #     imagem = imagem.convert("P")
-    #     imagem2 = Image.new("P",imagem.size,255)
-    #     for x in range(imagem.size[1]):
-    #         for y in range(imagem.size[0]):
-    #             cor_pixel = imagem.getpixel((y,x))
-    #             if cor_pixel < 127:
-    #                 imagem2.putpixel((y,x),0)
-    #     nome_arquivo = os.path.basename(arquivo)
-    #     imagem2.save(f'{destino}/{nome_arquivo}')
+    
         
 def getLetrasAdaptativo(origem= None,destino=None):
     arquivos = glob.glob(f'{origem}/*')
